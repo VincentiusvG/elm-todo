718426fb3f18bf7fcaed6325e9b7539e5f316ef3
Vincent <gitkraken@van.gameren.net>
2017-10-25 14:43:28 +0200

    add model view and update

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,43 @@
 module Main exposing (..)
 
---remove comments to fix your first elm issue
---import Html exposing (..)
+import Html exposing (..)
 
 
+--should be useful in a few minutes:
+--import Html.Attributes exposing (..)
+--import Html.Events exposing (..)
+
+
+type Msg
+    = WhaitWutNoMessageHere
+
+
+model : { data : String }
+model =
+    { data = "world"
+    }
+
+
+view : { a | data : String } -> Html msg
+view model =
+    --say hello
+    text ("Hello " ++ model.data)
+
+
+update : a -> b -> b
+update msg model =
+    --update does not change model
+    model
+
+
+
+-- behold, the main part to glue it all together
+
+
+main : Program Never { data : String } msg
 main =
-    text "Hello world"
+    Html.beginnerProgram
+        { view = view
+        , update = update
+        , model = model
+        }
