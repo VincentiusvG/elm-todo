1f776216aaf96ddd3bb78647fb39ef4c0f20a3e6
Vincent <gitkraken@van.gameren.net>
2017-10-25 14:22:43 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Elm ToDo
+
+El todo is my demo project for showing elm and the elm architecture.
---
+++ src/Main.elm
@@ -0,0 +1,8 @@
+module Main exposing (..)
+
+--remove comments to fix your first elm issue
+--import Html exposing (..)
+
+
+main =
+    text "Hello world"
